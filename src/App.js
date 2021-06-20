13603e8455b6bed6de16d807825c4b4e8faeecf6
Udenna Nebeolisa <66294750+sir-udenna@users.noreply.github.com>
2021-06-20 10:29:28 -0500

    browser router integration
    Browser router integration for different pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import './App.css';
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>h1</h1>
-      </header>
+    <Router>
+    <div>
+      <ul className="navbar">
+        <li>
+          <Link to="/">Dashboard</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/getstarted">Get started</Link>
+        </li>
+      </ul>
+
+      <Switch>
+        <Route path="/about">
+          <h1>project description</h1>
+        </Route>
+        <Route path="/getstarted">
+          <h1>Get started</h1>
+        </Route>
+        <Route path="/">
+          <h1>Dashboard</h1>
+        </Route>
+      </Switch>
     </div>
+  </Router>
   );
 }
 
